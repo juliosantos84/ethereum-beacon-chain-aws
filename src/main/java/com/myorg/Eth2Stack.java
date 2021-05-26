4a6e416a6fc42b1bde6b08a38f955496fbc89f5f
Julio Santos <julio.santos@venmo.com>
2021-05-25 21:54:47 -0400

    Rework NLB and add security group to ASG.

--- src/main/java/com/myorg/Eth2Stack.java
+++ src/main/java/com/myorg/Eth2Stack.java
@@ -1,18 +1,37 @@
 package com.myorg;
 
+import java.util.Arrays;
+import java.util.HashMap;
+
 import software.amazon.awscdk.core.Construct;
 import software.amazon.awscdk.core.Stack;
 import software.amazon.awscdk.core.StackProps;
+import software.amazon.awscdk.services.autoscaling.AutoScalingGroup;
+import software.amazon.awscdk.services.ec2.InstanceClass;
+import software.amazon.awscdk.services.ec2.InstanceSize;
+import software.amazon.awscdk.services.ec2.InstanceType;
+import software.amazon.awscdk.services.ec2.MachineImage;
+import software.amazon.awscdk.services.ec2.Peer;
+import software.amazon.awscdk.services.ec2.Port;
+import software.amazon.awscdk.services.ec2.SecurityGroup;
 import software.amazon.awscdk.services.ec2.SubnetSelection;
 import software.amazon.awscdk.services.ec2.SubnetType;
 import software.amazon.awscdk.services.ec2.Vpc;
+import software.amazon.awscdk.services.elasticloadbalancingv2.AddNetworkTargetsProps;
+import software.amazon.awscdk.services.elasticloadbalancingv2.NetworkListener;
 import software.amazon.awscdk.services.elasticloadbalancingv2.NetworkListenerProps;
 import software.amazon.awscdk.services.elasticloadbalancingv2.NetworkLoadBalancer;
+import software.amazon.awscdk.services.elasticloadbalancingv2.NetworkTargetGroup;
 import software.amazon.awscdk.services.elasticloadbalancingv2.Protocol;
 
 
 
 public class Eth2Stack extends Stack {
+
+    public static final Integer GOETH_PORT = Integer.valueOf(30303);
+    public static final Integer LIGHTHOUSE_PORT = Integer.valueOf(9000);
+    public static final Integer GRAFANA_PORT = Integer.valueOf(3000);
+
     public Eth2Stack(final Construct scope, final String id) {
         this(scope, id, null);
     }
@@ -21,51 +40,78 @@ public class Eth2Stack extends Stack {
         super(scope, id, props);
 
         // Configure a VPC
-        Vpc ethVpc = Vpc.Builder.create(this, "EthVpc")
+        Vpc vpc = Vpc.Builder.create(this, "EthVpc")
             .cidr("10.1.0.0/16")
             .subnetConfiguration(Vpc.DEFAULT_SUBNETS)
             .maxAzs(Integer.valueOf(3))
             .build();
-        
+
         // Configure a load balancer and ec2 ASG
         NetworkLoadBalancer publicLb = NetworkLoadBalancer.Builder.create(this, "publicLb")
-            .vpc(ethVpc)
+            .vpc(vpc)
             .vpcSubnets(
                 SubnetSelection.builder().subnetType(SubnetType.PUBLIC).build()
             )
             .internetFacing(Boolean.TRUE)
+            .crossZoneEnabled(Boolean.TRUE)
             .build();
 
-        // Go Eth Listener
-        publicLb.addListener("goEth", 
-            getNetworkListenerProps(Protocol.TCP_UDP, 30303)
+        SecurityGroup backendAsgSecurityGroup = SecurityGroup.Builder.create(this, "backendAsgSecurityGroup")
+            .vpc(vpc)
+            .build();
+
+        backendAsgSecurityGroup.addIngressRule(Peer.ipv4("10.1.0.0/16"), Port.tcp(GOETH_PORT));
+        backendAsgSecurityGroup.addIngressRule(Peer.ipv4("10.1.0.0/16"), Port.udp(GOETH_PORT));
+        backendAsgSecurityGroup.addIngressRule(Peer.ipv4("10.1.0.0/16"), Port.tcp(80)); // TEST
+
+        // Autoscaling group for ETH backend
+        AutoScalingGroup backendAsg = AutoScalingGroup.Builder.create(this, "backendAsg")
+            .vpc(vpc)
+            .instanceType(InstanceType.of(InstanceClass.BURSTABLE3_AMD, InstanceSize.LARGE))
+            .desiredCapacity(Integer.valueOf(1))
+            .minCapacity(Integer.valueOf(1))
+            .allowAllOutbound(Boolean.TRUE)
+            .securityGroup(backendAsgSecurityGroup)
+            .vpcSubnets(SubnetSelection.builder().subnetType(SubnetType.PRIVATE).build())
+            .machineImage(MachineImage.genericLinux(
+                new HashMap<String, String>(){
+                {
+                    put("us-east-1", "ami-0db6c6238a40c0681");
+                    put("us-east-2", "ami-03b6c8bd55e00d5ed");
+                }
+            }))
+            .build();
+
+        NetworkListener goEthListener = publicLb.addListener("goEth", 
+            NetworkListenerProps.builder()
+                .protocol(Protocol.TCP_UDP)
+                .port(GOETH_PORT)
+                .loadBalancer(publicLb)
+                .build()
+        );        
+
+        NetworkListener testListener = publicLb.addListener("test", 
+            NetworkListenerProps.builder()
+                .protocol(Protocol.TCP_UDP)
+                .port(Integer.valueOf(80))
+                .loadBalancer(publicLb)
+                .build()
+        );    
+
+        NetworkTargetGroup goEthTargetGroup = goEthListener.addTargets("ethBackendTargets", 
+            AddNetworkTargetsProps.builder()
+                .targets(Arrays.asList(backendAsg))
+                .port(GOETH_PORT)
+                .build()
         );
 
-        // Lighthouse Listener
-        publicLb.addListener("lighthouse", 
-            getNetworkListenerProps(Protocol.TCP_UDP, 9000)
+        NetworkTargetGroup testTargetGroup = testListener.addTargets("testTargets", 
+            AddNetworkTargetsProps.builder()
+                .targets(Arrays.asList(backendAsg))
+                .port(Integer.valueOf(80))
+                .build()
         );
-
-        // grafana Listener
-        publicLb.addListener("grafana", 
-            getNetworkListenerProps(Protocol.TCP_UDP, 3000)
-        );
-
-        // Instance eth1Client = Instance.Builder.create(this, "Eth1Client")
-        //     .instanceType(InstanceType.of(InstanceClass.BURSTABLE3_AMD, InstanceSize.LARGE))
-        //     .machineImage(MachineImage.genericLinux(
-        //         new HashMap<String, String>(){
-        //         {
-        //             put("us-east-1", "ami-0db6c6238a40c0681");
-        //             put("us-east-2", "ami-03b6c8bd55e00d5ed");
-        //         }
-
-        //     }))
-        //     .vpc(ethVpc)
-        //     .vpcSubnets(
-        //         SubnetSelection.builder().subnetType(SubnetType.PRIVATE).build()
-        //     )
-        //     .build();
+        
     }
 
     public static NetworkListenerProps getNetworkListenerProps(Protocol protocol, Integer port) {
