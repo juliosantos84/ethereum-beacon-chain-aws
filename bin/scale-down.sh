6f21b41cbee3eefb5ba391a5bdf6be3030fe842f
Julio Santos <julio.santos@venmo.com>
2021-05-31 06:50:42 -0400

    Working self-bootstrapping service.

---
+++ bin/scale-down.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+export ASG_DESIRED_CAPACITY=0
+$(dirname $0)/set-desired-capacity.sh
---
+++ bin/scale-up.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+export ASG_DESIRED_CAPACITY=1
+$(dirname $0)/set-desired-capacity.sh
