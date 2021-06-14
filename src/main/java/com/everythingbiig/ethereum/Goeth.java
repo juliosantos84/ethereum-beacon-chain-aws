a64e7b6aab46730f2f6873f93f680d093e83aa94
Julio Santos <julio.santos@venmo.com>
2021-06-13 21:25:02 -0400

    Update goeth ami.

--- src/main/java/com/everythingbiig/ethereum/Goeth.java
+++ src/main/java/com/everythingbiig/ethereum/Goeth.java
@@ -52,7 +52,7 @@ import software.amazon.awscdk.services.route53.targets.LoadBalancerTarget;
 public class Goeth extends Stack {
 
     public static final IMachineImage GOETH_AMI         = MachineImage.lookup(
-        LookupMachineImageProps.builder().name("goeth-20210613235747").build());
+        LookupMachineImageProps.builder().name("goeth-20210614012218").build());
     static final Integer    GOETH_PORT                  = Integer.valueOf(30303);
     static final Integer    GOETH_RPC_PORT                  = Integer.valueOf(8545);
     static final Integer    GRAFANA_PORT                = Integer.valueOf(3000);
