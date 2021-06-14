00230f3b3dbc72d618834ba27157e9057c64a344
Julio Santos <julio.santos@venmo.com>
2021-06-13 21:40:28 -0400

    Fix volume attachment logic.

--- src/main/resources/bin/attach-goeth-volume.sh
+++ src/main/resources/bin/attach-goeth-volume.sh
@@ -2,7 +2,8 @@
 
 REGION=$(curl http://169.254.169.254/latest/meta-data/placement/region)
 AVAILABILITY_ZONE=$(curl http://169.254.169.254/latest/meta-data/placement/availability-zone)
-VOLUME_ID=$(aws ec2 describe-volumes --filters Name=tag:Name,Values=chaindata Name=availability-zone,Values=${AVAILABILITY_ZONE} --region ${REGION} | jq -r '.Volumes[].VolumeId')
+VOLUME_NAME_TAG=$(cat /home/ubuntu/volume-name-tag)
+VOLUME_ID=$(aws ec2 describe-volumes --filters Name=tag:Name,Values=${VOLUME_NAME_TAG} Name=availability-zone,Values=${AVAILABILITY_ZONE} --region ${REGION} | jq -r '.Volumes[].VolumeId')
 
 echo "Attaching ${VOLUME_ID}..." && aws ec2 attach-volume --device /dev/sdd \
 --instance-id $(curl http://169.254.169.254/latest/meta-data/instance-id) \
--- src/main/resources/bin/detach-goeth-volume.sh
+++ src/main/resources/bin/detach-goeth-volume.sh
@@ -2,6 +2,7 @@
 
 REGION=$(curl http://169.254.169.254/latest/meta-data/placement/region)
 VOLUME_NAME_TAG=$(cat /home/ubuntu/volume-name-tag)
+AVAILABILITY_ZONE=$(curl http://169.254.169.254/latest/meta-data/placement/availability-zone)
 VOLUME_ID=$(aws ec2 describe-volumes --filters Name=tag:Name,Values=${VOLUME_NAME_TAG} Name=availability-zone,Values=${AVAILABILITY_ZONE} --region ${REGION} | jq -r '.Volumes[].VolumeId')
 INSTANCE_ID=$(curl http://169.254.169.254/latest/meta-data/instance-id)
 
