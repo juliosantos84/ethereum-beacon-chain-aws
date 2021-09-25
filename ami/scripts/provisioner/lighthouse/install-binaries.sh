9437978bf933ea97b929ba2e25dd3e51aec20333
Julio Santos <julio.santos@venmo.com>
2021-09-25 09:43:28 -0400

    Upgrade lighthouse to latest stable release.

--- ami/scripts/provisioner/lighthouse/install-binaries.sh
+++ ami/scripts/provisioner/lighthouse/install-binaries.sh
@@ -4,23 +4,23 @@ set -e
 
 set -x
 
-LIGHTHOUSE_VERSION=v1.3.0
+LIGHTHOUSE_VERSION=v1.5.2
 
-LIGHTHOUSE_ARCHIVE=lighthouse-v1.3.0-x86_64-unknown-linux-gnu.tar.gz
+LIGHTHOUSE_ARCHIVE=lighthouse-${LIGHTHOUSE_VERSION}-x86_64-unknown-linux-gnu.tar.gz
 
-LIGHTHOUSE_DOWNLOAD_URL=https://github.com/sigp/lighthouse/releases/download/v1.3.0/lighthouse-v1.3.0-x86_64-unknown-linux-gnu.tar.gz
+LIGHTHOUSE_DOWNLOAD_URL=https://github.com/sigp/lighthouse/releases/download/${LIGHTHOUSE_VERSION}/${LIGHTHOUSE_ARCHIVE}
 
-LIGHTHOUSE_SIG_FILE=${LIGHTHOUSE_ARCHIVE}.asc
+# LIGHTHOUSE_SIG_FILE=${LIGHTHOUSE_ARCHIVE}.asc
 
-LIGHTHOUSE_SIGNATURE_URL=https://github.com/sigp/lighthouse/releases/download/v1.3.0/lighthouse-v1.3.0-x86_64-unknown-linux-gnu.tar.gz.asc
+# LIGHTHOUSE_SIGNATURE_URL=https://github.com/sigp/lighthouse/releases/download/v1.3.0/lighthouse-v1.3.0-x86_64-unknown-linux-gnu.tar.gz.asc
 
-SIGMA_PRIME_PGP_KEY=15E66D941F697E28F49381F426416DC3F30674B0
+# SIGMA_PRIME_PGP_KEY=15E66D941F697E28F49381F426416DC3F30674B0
 
 DOWNLOAD_DIR=/tmp
 
 curl -L ${LIGHTHOUSE_DOWNLOAD_URL} --output ${DOWNLOAD_DIR}/${LIGHTHOUSE_ARCHIVE}
 
-curl -L ${LIGHTHOUSE_SIGNATURE_URL} --output ${DOWNLOAD_DIR}/${LIGHTHOUSE_SIG_FILE}
+# curl -L ${LIGHTHOUSE_SIGNATURE_URL} --output ${DOWNLOAD_DIR}/${LIGHTHOUSE_SIG_FILE}
 
 # verify 
 # gpg --receive-keys ${SIGMA_PRIME_PGP_KEY} \
