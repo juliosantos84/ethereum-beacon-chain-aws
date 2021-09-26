fe5810223c20dc2a5cd940119c64939f24117a0f
Julio Santos <julio.santos@venmo.com>
2021-09-26 07:56:10 -0400

    Fix host address lookup for geth service.

--- ami/scripts/provisioner/geth/install-service.sh
+++ ami/scripts/provisioner/geth/install-service.sh
@@ -4,4 +4,32 @@ set -e
 
 set -x
 
-sudo mv /tmp/units/geth.service /etc/systemd/system/geth.service
+HTTP_ADDR=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
+
+cat <<UNIT >> ./geth.service
+[Unit]
+Description=Ethereum go client
+After=network.target 
+Wants=network.target
+
+[Service]
+User=goeth 
+Group=goeth
+Type=simple
+Restart=always
+RestartSec=30
+
+ExecStartPre=/usr/local/bin/attach-goeth-volume.sh
+ExecStartPre=+/usr/local/bin/format-goeth-volume.sh
+ExecStartPre=+/usr/local/bin/mount-goeth-volume.sh
+
+ExecStart=geth --goerli --http --http.addr ${HTTP_ADDR} --http.api net,eth,web3 --http.vhosts * --datadir /var/lib/goethereum
+
+ExecStopPost=-+/usr/local/bin/unmount-goeth-volume.sh
+ExecStopPost=-+/usr/local/bin/detach-goeth-volume.sh
+
+[Install]
+WantedBy=default.target
+UNIT
+
+sudo mv ./geth.service /etc/systemd/system/geth.service
