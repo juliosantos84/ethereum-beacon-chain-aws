b19c66de70730a872afe8321ef20d779bb87819f
Julio Santos <julio.santos@venmo.com>
2021-09-26 10:10:08 -0400

    Update lighthouse AMI.

--- src/main/java/com/everythingbiig/ethereum/Lighthouse.java
+++ src/main/java/com/everythingbiig/ethereum/Lighthouse.java
@@ -47,7 +47,7 @@ import software.amazon.awscdk.services.route53.targets.LoadBalancerTarget;
 public class Lighthouse extends Stack {
     public static final IMachineImage LIGHTHOUSE_AMI = MachineImage.lookup(
         LookupMachineImageProps.builder()
-            .name("lighthouse-20210925134835").build());
+            .name("lighthouse-20210926140521").build());
     private AutoScalingGroup lighthouseAsg = null;
     private SecurityGroup lighthouseSecurityGroup = null;
     private List<IVolume> lighthouseVolumes        = null;
