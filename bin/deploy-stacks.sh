1aace356fd6fb6c70d7ca1240e6c88bdab76e3ab
Julio Santos <julio.santos@venmo.com>
2021-11-02 22:21:15 -0400

    Update to use AMI with services that support .env files.

--- bin/deploy-stacks.sh
+++ bin/deploy-stacks.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
 
+VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
 export BEACON_CHAIN_NETWORK=${1:-"testnet"}
 # export CDK_DEPLOY_ACCOUNT=""
 # export CDK_DEPLOY_REGION=""
-echo "Deploying beacon chain on ${BEACON_CHAIN_NETWORK}"
+echo "Deploying beacon chain ${VERSION} on ${BEACON_CHAIN_NETWORK}"
 cdk deploy --all --require-approval never
